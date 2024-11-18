745d6fb72264c382b084edd67ce862ae1f2bbd6b
Mirko Rinke <mirkorinke@htp.com>
2024-11-18 19:47:08 +0100

    add footer component with social media links and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { HowToOrderComponent } from './how-to-order/how-to-order.component';
 import { OurMenuComponent } from './our-menu/our-menu.component';
 import { FindUsAtComponent } from './find-us-at/find-us-at.component';
 import { GoogleMapsComponent } from './google-maps/google-maps.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,7 @@ import { GoogleMapsComponent } from './google-maps/google-maps.component';
     OurMenuComponent,
     FindUsAtComponent,
     GoogleMapsComponent,
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',

0eeff9deb2fcc149f28ca6736447112af6876e82
Mirko Rinke <mirkorinke@htp.com>
2024-11-18 18:56:05 +0100

    add Find Us At section with location details and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,17 @@ import { Component } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { HowToOrderComponent } from './how-to-order/how-to-order.component';
 import { OurMenuComponent } from './our-menu/our-menu.component';
+import { FindUsAtComponent } from './find-us-at/find-us-at.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroSectionComponent, HowToOrderComponent, OurMenuComponent],
+  imports: [
+    HeroSectionComponent,
+    HowToOrderComponent,
+    OurMenuComponent,
+    FindUsAtComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

190d4574899d324c2a3366fee2f87f937d5feec3
Mirko Rinke <mirkorinke@htp.com>
2024-11-20 15:44:29 +0100

    add privacy policy component and update footer links for compliance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { FooterComponent } from './footer/footer.component';
-import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, MainContentComponent],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
